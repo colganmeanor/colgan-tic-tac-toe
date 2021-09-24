5e2034001f8ea4ba0b854a19faa4058bc82db3cc
Colgan Meanor <colganmeanor7@gmail.com>
2021-09-23 22:27:17 -0500

    Add rough draft comp

---
+++ player.js
@@ -0,0 +1,19 @@
+class Player{
+  constructor(){
+    this.token: 'token'
+    this.id =  'ID'
+    this.wins = [];
+  }
+
+  saveWinsToStorage(){
+
+  }
+
+  retrieveWinsFromStorage(){
+
+  }
+
+}
+
+
+module.exports = Player
